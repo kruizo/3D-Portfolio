6d25435a92af9613bab7ad5e0c314d0e3412ccaa
kruizo <kynethruizo@gmail.com>
2026-01-01 21:58:33 +0800

    fix: updated cv

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -59,7 +59,7 @@ function About() {
             HTML/CSS, I bring a comprehensive skill set to every project.
           </p>
           <div className="flex gap-4 py-5 my-5 ">
-            <a href="../assets/Resume.pdf" download className="z-50">
+            <a href="../../public/Resume.pdf" download className="z-50">
               <button
                 type="button"
                 className="bg-secondary z-50 text-slate-10 dark:text-white hover:text-black hover:dark:text-black transition-colors hover:bg-white py-3 px-5"
